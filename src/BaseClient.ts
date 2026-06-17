23f0eadc748fd8b58c6135356855733fb92971dc
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-06-17 10:16:35 +0000

    Release 0.0.47

--- src/BaseClient.ts
+++ src/BaseClient.ts
@@ -59,8 +59,8 @@ export function normalizeClientOptions<T extends BaseClientOptions = BaseClientO
         {
             "X-Fern-Language": "JavaScript",
             "X-Fern-SDK-Name": "@yasminaai/motor-typescript",
-            "X-Fern-SDK-Version": "0.0.45",
-            "User-Agent": "@yasminaai/motor-typescript/0.0.45",
+            "X-Fern-SDK-Version": "0.0.47",
+            "User-Agent": "@yasminaai/motor-typescript/0.0.47",
             "X-Fern-Runtime": core.RUNTIME.type,
             "X-Fern-Runtime-Version": core.RUNTIME.version,
         },
